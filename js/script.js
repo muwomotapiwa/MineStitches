daec73b58aac00a9f61928cbc8a228549ba29223
muwomotapiwa <muwomotapiwa@gmail.com>
2025-04-12 19:42:59 +0200

    sfsf

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 document.querySelector('.scroll-top').addEventListener('click', () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+// Select DOM elements
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+// Add click event listener to toggle the dropdown
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
